f1e870f1ad6656ec025d9c0897eead54891ceca3
lucachiossi <engr.luca.chiossi@gmail.com>
2021-03-15 13:55:32 +0100

    changed title to Sentiment Analyzer

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -5,6 +5,10 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
+  componentDidMount(){
+    document.title = "Sentiment Analyzer"
+  }
+
   state = {innercomp:<textarea rows="4" cols="50" id="textinput"/>,
             mode: "text",
           sentimentOutput:[],
